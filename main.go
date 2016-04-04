a26af735e64c239228dc1b5198a2dac7f4fcfa65
Joe Kratzat <joe.kratzat@gmail.com>
2016-04-03 21:39:25 -0400

    Setup simple config file
    Why:
    For some of the commands it will make sense to use
    default settings. Having those stored in a config file
    makes sense.

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/user"
+	"strings"
 
 	"github.com/codegangsta/cli"
 	"github.com/foxio/john_foxio_cli/command"
 )
+
+var (
+	configurationFile = ".john_foxio"
+)
+
+// Configuration represents the config file
+type Configuration struct {
+	Version string
+}
 
 func main() {
 	app := cli.NewApp()
@@ -14,7 +28,19 @@ func main() {
 	app.Version = "0.0.1"
 	app.Action = command.Default
 
+	if firstTimeSetup() {
+		writeConfigurationFile(app)
+	} else {
+		c := readConfiguration()
+		log.Println(c)
+	}
+
 	app.Commands = []cli.Command{
+		{
+			Name:   "update",
+			Usage:  "Update Johnny",
+			Action: update,
+		},
 		{
 			Name:    "init",
 			Aliases: []string{"in"},
@@ -24,4 +50,54 @@ func main() {
 	}
 
 	app.Run(os.Args)
+}
+
+func update(c *cli.Context) {
+	println("Updating ... ")
+	writeConfigurationFile(c.App)
+	println("Updating complete")
+}
+
+func firstTimeSetup() bool {
+	if _, err := os.Stat(configurationFilePath()); os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
+func readConfiguration() *Configuration {
+	dat, err := ioutil.ReadFile(configurationFilePath())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var c Configuration
+	err = json.Unmarshal(dat, &c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &c
+}
+
+func writeConfigurationFile(app *cli.App) {
+	configuration := Configuration{app.Version}
+	configurationJSON, err := json.Marshal(configuration)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(configurationFilePath(), configurationJSON, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func configurationFilePath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return strings.Join([]string{usr.HomeDir, configurationFile}, string(os.PathSeparator))
 }
