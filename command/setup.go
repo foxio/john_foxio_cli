7fec40dcc0f0cca4b9fada83232a0de36038858e
Joe Kratzat <joe.kratzat@gmail.com>
2017-11-19 23:13:21 -0500

    Update the setup command to create logging dir
    Why:
    The pom logging needs to make sure there are the correct directories.

--- command/setup.go
+++ command/setup.go
@@ -1,8 +1,48 @@
 package command
 
-import "github.com/codegangsta/cli"
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/codegangsta/cli"
+	"github.com/foxio/john_foxio_cli/lib"
+)
+
+var (
+	rootLogFolder = ".john_logs"
+)
 
 // InitUser handles setting up the user defaults
 func InitUser(c *cli.Context) {
 	println("TODO: init task")
+	createLogDirs()
+}
+
+func createLogDirs() {
+	homePath, err := lib.HomeDir()
+	if err != nil {
+		log.Println("Could not find home dir: ", err)
+		return
+	}
+
+	logDirs := []string{lib.PomLogDir}
+
+	for _, logDir := range logDirs {
+		dir := filepath.Join(homePath, lib.RootLogFolder, logDir)
+		if shouldCreateDir(dir) {
+			createLogDir(dir)
+		}
+	}
+}
+
+func shouldCreateDir(dir string) bool {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
+func createLogDir(logDir string) {
+	os.MkdirAll(logDir, os.ModePerm)
 }
